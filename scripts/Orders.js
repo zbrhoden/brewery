37d9f449464a30d8c90c46c6ec41bd9126d058ad
Blake Rhoden <zbrhoden@gmail.com>
2021-08-12 13:33:54 -0500

    first commit

---
+++ scripts/Orders.js
@@ -0,0 +1,48 @@
+import { getProducts, getEmployees, getOrders } from "./database.js"
+
+// Get copy of state for use in this module
+const products = getProducts()
+const employees = getEmployees()
+const orders = getOrders()
+
+
+// Function whose responsibility is to find the product for an order
+const findProduct = (order, allProducts) => {
+    let orderProduct = ""
+
+    for (const product of allProducts) {
+        if (product.id === order.productId) {
+            orderProduct = product
+        }
+    }
+
+    return orderProduct
+}
+
+// Function whose responsibility is to find the employee for an order
+const findEmployee = (order, allEmployees) => {
+    let orderEmployee = ""
+
+    for (const employee of allEmployees) {
+        if (employee.id === order.employeeId) {
+            orderEmployee = employee
+        }
+    }
+    return orderEmployee
+}
+
+export const Orders = () => {
+    let html = "<ul>"
+
+    for (const order of orders) {
+        console.log ("this order:", order)
+        const employee = findEmployee(order, employees)
+        const product = findProduct(order, products)
+        html += `<li>${product.name} was sold by ${employee.name} on ${new Date(order.timestamp).toLocaleDateString()}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
